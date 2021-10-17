2b600be59fb215c543c6604c3aa6dfc0b1373ba9
Alexey Barkun <alexeyb@playtika.com>
2021-10-18 00:15:56 +0300

    hw3595

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import fs from 'fs'
 import variants from './variants.json'
 import bodyParser from 'body-parser'
 
-fs.writeFileSync('pid',process.pid.toString())
-
+fs.writeFileSync('pid', process.pid.toString())
 const app = express();
 const PORT = 3000;
+const variantsTyped = variants as { [key: string]: string }
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +14,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/variants', (req, res) => {
     res.json(variants);
 });
+app.get('/stat', (req, res) => {
+    const obj = fs.readFileSync('results.json');
+    switch (req.headers.accept) {
+        case 'json':
+            res.send(obj)
+            break;
+        case 'xml':
+            const xml = convertToXML(JSON.parse(obj.toString()))
+            res.send(xml);
+            break;
+        case 'html':
+            const html = convertToHTNL(JSON.parse(obj.toString()))
+            res.send(html);
+            break;
+        default:
+            res.status(404).end()
+            break;
+    }
+    res.end()
 
+})
 app.post('/stat', (req, res) => {
     const obj = JSON.parse(fs.readFileSync('results.json').toString());
     res.json(obj);
@@ -35,4 +55,21 @@ app.post('/vote', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+function convertToXML(json: { [key: string]: number }) {
+    var xml = '<results>';
+    for (const id in json) {
+        xml += `<${id}>${json[id]}</${id}>`;
+    };
+    xml += '</results>';
+    return xml
+}
+function convertToHTNL(json: { [key: string]: number }) {
+    var html = '<div>';
+    for (const id in json) {
+        html += `<div>${variantsTyped[id]}</div><div>${json[id] || 0}</div><Br>`;
+    };
+    html += '</div>';
+    return html
+}
 
