baa16b826e679d19b8cb8de3311f6b7332cac75c
Alexey Barkun <alexeyb@playtika.com>
2021-10-21 16:16:51 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -45,20 +45,21 @@ app.post<'/vote', {}, {}, IKeyStringString>('/vote', (req, res) => {
     const voteId = req.body.voteId;
     const origin = req.headers.origin;
     //if (origin) res.setHeader("Location", origin)
+    const queryVoteIdtext = `?voteId=${voteId}`
     try {
         if (variants.hasOwnProperty(voteId)) {
             const obj = JSON.parse(fs.readFileSync('./results.json').toString());
             obj[voteId] += 1;
             fs.writeFileSync('./results.json', JSON.stringify(obj));
             //ВОТ ЭТОТ ВАРИАНТ ПОВТОРНО ГОЛОСУЕТ ПРИ ОБНОВЛЕНИИ СТРАНИЦЫ
-            //res.sendFile(path.resolve('public/index.html'))  
-            res.redirect('/')
+            //res.sendFile(path.resolve('public/index.html'))
+            res.redirect(`/${queryVoteIdtext}`)
         } else {
-            res.redirect('/error')
+            res.redirect(`/error${queryVoteIdtext}`)
         }
 
     } catch (error) {
-        res.redirect('/error')
+        res.redirect(`/error${queryVoteIdtext}`)
     }
 });
 
